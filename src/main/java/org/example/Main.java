69d1fcf7af3e67d399e522893912657d7c05c655
Mertcan Mert <mertcan.mert@yahoo.com>
2026-02-21 18:02:57 +0300

    Test Başarılı

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,55 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (!isBarking || clock < 0 || clock > 23) {
+            return false;
+        } else if (clock >= 20 || clock < 8) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+            if (firstAge>= 13 && firstAge <= 19) {
+                return true;
+            } else if (secondAge >= 13 && secondAge <= 19) {
+                return true;
+            } else if (thirdAge >= 13 && thirdAge <= 19) {
+                return true;
+            } else {
+                return false;
+            }
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        int maxTemp;
+        if (isSummer) {
+            maxTemp = 45;
+        } else {
+            maxTemp = 35;
+        }
+
+        if (temp >= 25 && temp <= maxTemp) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static double area(double width, double height) {
-       
-    }
+        if (width < 0 || height < 0) {
+            return -1;
+    }   else {
+            return width * height;
+        }
+}
 
     public static double area(double radius) {
-       
+        if (radius < 0) {
+            return -1;
+        }
+        return radius * radius * Math.PI;
     }
 }
+
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.Main;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
